c3a16ec11e6d4b155eb0a7791a5ec7d8f4c83469
krzysztof <krzszk96@gmail.com>
2022-06-03 22:17:11 +0200

    count and save user clicks

--- src/js/module.js
+++ src/js/module.js
@@ -1,7 +1,8 @@
 const close = document.getElementById('alert-popup')
 const back = document.getElementById('tint-background')
 const displayClicks = document.getElementById('displayclicks')
-var userclicks = 0
+var userclicks =0
+userclicks = localStorage.getItem('userclicks', userclicks);
 
 function closePopup(){  
   close.style.display = "none";
@@ -11,12 +12,18 @@ function showPopup(){
   close.style.display = "block";
   back.style.display = "block";
   countClicks()
+  updateClicks()
 }
-
 back.addEventListener("click", ()=> {
   closePopup()
 })
-
-function countClicks(){
+function countClicks(){ 
   userclicks++
+  saveClicks()
+}
+function saveClicks(){  
+  localStorage.setItem('userclicks', userclicks);
+}
+function updateClicks(){
+  displayClicks.innerHTML = `${userclicks} times`
 }
