c28fa748035fe786bb1bd016a0e949b2a7ef8fb3
krzysztof <krzszk96@gmail.com>
2022-06-03 22:35:05 +0200

    popup logic complete

--- src/js/module.js
+++ src/js/module.js
@@ -1,18 +1,21 @@
 const close = document.getElementById('alert-popup')
 const back = document.getElementById('tint-background')
 const displayClicks = document.getElementById('displayclicks')
+const moreclicks = document.getElementById('moreclicks')
 var userclicks =0
-userclicks = localStorage.getItem('userclicks', userclicks);
+userclicks = localStorage.getItem('userclicks')
 
-function closePopup(){  
-  close.style.display = "none";
-  back.style.display = "none";
-}
+
 function showPopup(){
-  close.style.display = "block";
-  back.style.display = "block";
+  close.style.display = "block"
+  back.style.display = "block"
   countClicks()
   updateClicks()
+  checkNumberOfClicks()
+}
+function closePopup(){  
+  close.style.display = "none"
+  back.style.display = "none"
 }
 back.addEventListener("click", ()=> {
   closePopup()
@@ -22,8 +25,18 @@ function countClicks(){
   saveClicks()
 }
 function saveClicks(){  
-  localStorage.setItem('userclicks', userclicks);
+  localStorage.setItem('userclicks', userclicks)
 }
 function updateClicks(){
   displayClicks.innerHTML = `${userclicks} times`
+}
+function checkNumberOfClicks(){
+  let clicks = localStorage.getItem('userclicks', userclicks)
+  if(clicks > 5) moreclicks.style.display = "block"
+}
+function resetClicks(){
+  localStorage.removeItem('userclicks');
+  userclicks =0
+  moreclicks.style.display = "none"
+  updateClicks()
 }
